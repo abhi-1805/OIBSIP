598abdd013d474802fb0a6640ef4eb2ab1fb010c
abhi-1805 <135457918+abhi-1805@users.noreply.github.com>
2023-10-27 17:55:26 +0530

    Add files via upload

---
+++ Temperature converter/script.js
@@ -0,0 +1,33 @@
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+let btn = document.querySelector('.button button');
+
+
+function computeTemp(event) {
+  const currentValue = +event.target.value;
+
+  switch (event.target.name) {
+    case "celsius":
+      kelvinEl.value = (currentValue + 273.32).toFixed(2);
+      fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+      break;
+    case "fahrenheit":
+      celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+      break;
+    case "kelvin":
+      celsiusEl.value = (currentValue - 273.32).toFixed(2);
+      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+      break;
+    default:
+      break;
+  }
+}
+
+
+btn.addEventListener('click', ()=>{
+    celsiusEl.value = ""
+    fahrenheitEl.value = ""
+    kelvinEl.value = ""
+})
